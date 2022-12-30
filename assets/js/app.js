e26639046537db180d5a11dc31885a3f01be930b
cc-aeya <aaminah.x@icloud.com>
2022-12-30 02:09:11 +0000

    chnaged all html added css

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+
+
+
+var apiKey = '753dbaeb28d5f9e47d1357c0fbe984e8';
+var baseURL = 'https://api.openweathermap.org/data/2.5/';
+var weatherURL = baseURL + `weather?appid=${apiKey}&units=metric&`;
+var forecastURL = baseURL + `forecast?appid=${apiKey}&units=metric&`;
