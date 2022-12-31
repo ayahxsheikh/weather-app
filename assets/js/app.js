989d28aa556664a0e84a2aab2abacb5eedaa78b9
cc-aeya <aaminah.x@icloud.com>
2022-12-31 18:35:34 +0000

    psuedo code, global vars created

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,7 @@
 
-
 //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 //https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-
 
 
 
@@ -11,3 +9,29 @@ var apiKey = '753dbaeb28d5f9e47d1357c0fbe984e8';
 var baseURL = 'https://api.openweathermap.org/data/2.5/';
 var weatherURL = baseURL + `weather?appid=${apiKey}&units=metric&`;
 var forecastURL = baseURL + `forecast?appid=${apiKey}&units=metric&`;
+
+var mainWrapper = $('.main')
+var topSec = $('.top')
+var searchInput = $('#search-input')
+var asideEl = $('.search-form')
+
+// When key-enter is pressed
+    //show current weather
+    //show 5 day forecast
+    //show search history
+        //add city name to history
+        //pull from local storage
+        //if history is not empty, output each city to the webpage
+
+
+//create history-block with button-Els and append to asideEl
+//create weather-today block and append to topSec
+
+
+
+
+
+
+
+
+
